7aa81b06c15fe898b6c752c79c242fde3772fa23
Dean Araujo <deantarik4@gmail.com>
2024-03-25 12:04:10 -0300

    Product

---
+++ src/main/java/com/deanaraujo/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.deanaraujo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.deanaraujo.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
